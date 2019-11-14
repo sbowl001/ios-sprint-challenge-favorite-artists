f1d78a89a4a4272dfb1aa79ad0612c4a66189f40
Stephanie <sbowl001@fiu.edu>
2019-11-14 23:47:53 +0800

    category

--- Favorite artists/Favorite artists/LSIArtist+NSJSONSerialization.m
+++ Favorite artists/Favorite artists/LSIArtist+NSJSONSerialization.m
@@ -17,15 +17,27 @@
 {
     self = [super init];
     if (self) {
-        self.artist = [dictionary objectForKey:@"strArtist"];
-        self.bio = [dictionary objectForKey:@"strBiographyEN"];
-        self.year = [[dictionary objectForKey:@"intFormedYear"] intValue];
+        NSString *artistName = [dictionary objectForKey:@"strArtist"];
+        NSString *bio = [dictionary objectForKey:@"strBiographyEN"];
+        NSString *yearFormed = [dictionary objectForKey:@"intFormedYear"];
+        
+        if (artistName != nil && yearFormed != nil &&bio != nil){
+            self = [self initWithArtistName:artistName yearFormed:yearFormed bio:bio];
+        } else {
+            NSLog(@"Invalid artist: %@ JSON Object", artistName);
+            self = nil;
+        }
     }
     return self;
 }
 -(NSDictionary *)toDictionary
 {
-    
+    NSDictionary *result = @{
+        @"strArtist": self.artist,
+        @"strBiographyEN": self.bio,
+        @"intFormedYear": self.yearFormed
+    };
+    return result;
 }
 //Add a method that returns your model object as a NSDictionary (to save artists).
 
